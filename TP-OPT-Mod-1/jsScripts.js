f9819a656e4aed5987f57337677f7df98e67e6dc
chineseStuff <pablon.88@gmail.com>
2020-03-04 17:48:41 -0300

    Initial commit for TP Optativo Modulo 1

---
+++ TP-OPT-Mod-1/jsScripts.js
@@ -0,0 +1,6 @@
+function submitHandler(event) {
+  alert(
+    "En un futuro sus datos serán enviados a un asesor. Por ahora no haremos nada. Gracias por perder su tiempo"
+  );
+  event.preventDefault();
+}
